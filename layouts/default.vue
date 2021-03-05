7b087a390c480ff1531aac27861d62a335bddc7a
zeobxx <meowmeow.tbz@gmail.com>
2021-03-05 19:55:07 +0700

    s1

---
+++ layouts/default.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-toolbar-title text-align: center v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      @ jikin Api
+    </v-footer>
+      <v-carousel hide-delimiters>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      fixed: false,
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'ZEOBX_MANGA',
+      items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          },
+        ],
+    }
+  },
+}
+</script>
